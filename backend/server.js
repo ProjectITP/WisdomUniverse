90a4196216138c05996741007aa3f52b9753f598
uabinesh <79247690+uabinesh@users.noreply.github.com>
2021-09-07 17:10:50 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors= require("cors");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify:false
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+    console.log("MongoDB Connection success!");
+});
+
+app.listen(PORT, ()=>{
+    console.log('Server is up and running on port number: ',PORT)
+})
+
+/*const AssessmentRouter = require("./routes/Assessment.js");
+app.use("/Assessment",AssessmentRouter);*/
