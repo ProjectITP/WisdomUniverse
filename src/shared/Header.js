4d038511a84710c7e57f7fd0de374a7118682ced
uabinesh <79247690+uabinesh@users.noreply.github.com>
2021-09-10 00:01:23 +0530

    header update

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -3,51 +3,54 @@ import React from 'react'
 function Header(){
 
     return(
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#" style={{color:"red"}}>Wisdom Universe</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Notices</a> 
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Resources</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Subject</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Payements</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Assessments
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="">Quiz</a>
-                        <a class="dropdown-item" href="/i/quiz">View Quiz(I)</a>
-                        <a class="dropdown-item" href="">Add Quiz</a>
-                        <a class="dropdown-item" href="/i/quiz">View Quiz(S)</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Assignment</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-            </ul>
-            
-        </div>
-    </nav>
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/" style={{color:"red"}}>Wisdom Universe</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Notices</a> 
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Resources</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Subject</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/i/quiz">Payements</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Assessments
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="">Quiz</a></li>
+                                <li><a class="dropdown-item" href="/i/quiz">View Quiz(I)</a></li>
+                                <li><a class="dropdown-item" href="">Add Quiz</a></li>
+                                <li><a class="dropdown-item" href="/i/quiz">View Quiz(S)</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"/></li>
+                                <li><a class="dropdown-item" href="#">Assignment</a></li>
+                                <li class="dropdown-item">Assignment</li>
+                                <li><a class="dropdown-item" href="#">Assignment</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
     )
 }
 
